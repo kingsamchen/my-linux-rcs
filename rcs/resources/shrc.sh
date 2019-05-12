20d20207e61ed5716f5cdc9d4099594cb47fc706
Kingsley Chen <kingsamchen@gmail.com>
2019-05-12 20:56:39 +0800

    Update shrc

--- rcs/resources/shrc.sh
+++ rcs/resources/shrc.sh
@@ -1,11 +1,4 @@
 
-# Avoid being loaded twice in interactive shell.
-if [ -z "$_INIT_SH_LOADED" ]; then
-    _INIT_SH_LOADED=1
-else
-    return
-fi
-
 # Do not source if not in the interactive shell.
 case "$-" in
     *i*) ;;
@@ -13,7 +6,7 @@ case "$-" in
 esac
 
 if [ -n "$SSH_CLIENT" -a -n "$DISPLAY" ]; then
-    _RUNNING_FROM_SSH_X=1
+    _RUNNING_FROM_SSH_X=2
 fi
 
 if [ -n "$_RUNNING_FROM_SSH_X" ]; then
@@ -27,3 +20,16 @@ if [ -d "/usr/local/go" ]; then
     export GOPATH=$HOME/go
     export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 fi
+
+NODE_HOME="$HOME/Programs/node-v10.15.3-linux-x64"
+if [ -d $NODE_HOME ]; then
+    export PATH=$NODE_HOME/bin:$PATH
+fi
+
+LOCAL_BIN="$HOME/.local/bin"
+if [ -d $LOCAL_BIN ]; then
+    export PATH=$LOCAL_BIN:$PATH
+fi
+
+[[ -s "$HOME/.local/share/marker/marker.sh" ]] && source "$HOME/.local/share/marker/marker.sh"
+
